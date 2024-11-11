ffab011278cc07602c7749bf99b3fda5c0025a92
seele-ai <seele@stu.pku.edu.cn>
2024-11-11 17:22:33 +0800

    添加测试数据

--- js/main.js
+++ js/main.js
@@ -4,18 +4,8 @@ function change_time()
     const range_of_time = document.getElementById('range_of_time');
     time_value = range_of_time.value;
     document.getElementById("time_value").innerHTML = ("您改变了一下时间:" + time_value + "min");
-    update_charm(get_all_value());
+    update_chart(get_all_value());
 }
-
-// 改变微博范围
-function change_weibo()  
-{
-    const selection = document.getElementById("select_of_weibo");
-    weibo = selection.value;
-    document.getElementById("weibo_range").innerHTML = ("您改变了范围" + weibo);
-    update_charm(get_all_value());
-}
-
 
 
 // 获取用户选择的参数
@@ -23,21 +13,59 @@ function get_all_value()
 {   
     const range_of_time = document.getElementById('range_of_time');
     time_value = range_of_time.value;
-    const selection = document.getElementById("select_of_weibo");
-    weibo = selection.value;
     values = {
         "time_value": time_value,
-        "weibo":weibo
     };
     return values;
 }
 
 
 //更新图表
-function update_charm(values)
-{
-    document.getElementById("charm").innerHTML = 
-    ("参数已更新<br> 当前参数：<br> &emsp;等时圈时间：" + values["time_value"] + "min"
-        + "<br>&emsp;微博范围： " + values["weibo"]
-    );
+function update_chart(values)
+{   
+    var poidata
+    d3.json('json/test.json',function(a){
+        poidata = a
+
+
+        document.getElementById("chart").innerHTML = 
+        "参数已更新<br> 当前参数：<br> &emsp;等时圈时间：" + values["time_value"] + "min"
+         + "<br>&emsp;POI: " + poidata["index"]
+        + "<br>&emsp;data: " + poidata["pois"]
+        /*读取数据后绘图
+        svg = d3.select("#bar-chart").select("svg")
+        svg.attr("class","firstsvg")
+
+        var w = 200;
+        var h = 100;
+        
+        var xscale = d3.scale.linear()
+        .domain(poidata["pois"])
+        .range([0,w])
+        var yscale = d3.scale.linear()
+        .domain(0,d3.max(poidata["pois"]))
+        .range(h-20,45)
+
+        var xAxis = d3.svg.axis.scale(xscale).orient("bottom")
+
+        svg.select("g.xaxis").call(xAxis)
+        .attr("transform",`translate(0, ${h-20})`)
+        .selectAll("text")
+        .attr("transform","rotate(-14)")
+
+
+
+        var bars = svg.selectAll('rect.first_bar').data(poidata["pois"]);
+        bars.exit().remove();
+
+        bars.enter().append('rect').attr('class','city-bar');
+
+        bars
+        .attr("x",function(d){return xscale(d)-15})
+        .attr("y",function(d){return yscale(d)-10})
+        .attr("width",20)
+        .attr("height",function(d){return h - yscale(d) - 10})*/
+
+
+    });
 }
