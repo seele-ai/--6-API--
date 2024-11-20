20c228b4062f47643b8089b1540fc72059af9d84
seele-ai <seele@stu.pku.edu.cn>
2024-11-20 13:10:03 +0800

    筛选签到数前10的POI并更改其icon

--- js/main.js
+++ js/main.js
@@ -24,7 +24,39 @@ function update_chart(values)
 {   
     document.getElementById("bar-chart").innerHTML = "&emsp;等时圈时间：" + values["time_value"] + "min"
     + "<br>&emsp;等时圈内poi数目: " + positions.length + '<svg id="poisvg"><g class="xaxis"></g><g class="yaxis"></g></svg>' 
-
+    selected_positions = positions.sort(function(a,b){
+        return  b["checkin_user_num"] - a["checkin_user_num"] // 按签到数排序
+    }).slice(0,10) // 切片
     
+    selected_positions.forEach(a => 
+    {
+        const marker = new AMap.Marker({
+            position: [a.longitude, a.latitude],
+            title: a.title,
+            icon: "//vdata.amap.com/icons/b18/1/2.png",
+            map: map
+        }); // 筛选后的点标记
+
+        const infoWindow = new AMap.InfoWindow({
+            content: `<div>
+                        <h4 class="windowh4">${a.title}</h4>
+                        <p>该POI签到数: ${a.checkin_user_num}</p>
+                        <button type="button" class="poi_button" id="${a.title}"> 规划路径 </button>
+                      </div>`,
+            offset: new AMap.Pixel(0, -30)
+        }); // 筛选后的框
+        
+        marker.on('click', () => {
+            infoWindow.open(map, marker.getPosition());
+
+            const button = document.getElementById(`${a.title}`)
+            button.addEventListener("click", updateButton);
+        });
+
+        function updateButton(){
+            console.log(a.latitude)
+            console.log(a.longitude) // 这里调用路径规划API
+        }
+    });
 
 };
