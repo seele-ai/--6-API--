ffb098b6016a1d1eb522e183392055fbdde74672
seele-ai <seele@stu.pku.edu.cn>
2024-11-20 11:54:23 +0800

    调整页面布局，预留统计图空间

--- js/main.js
+++ js/main.js
@@ -22,50 +22,9 @@ function get_all_value()
 //更新图表
 function update_chart(values)
 {   
-    var poidata
-    d3.json('json/test.json',function(a){
-        poidata = a
+    document.getElementById("bar-chart").innerHTML = "&emsp;等时圈时间：" + values["time_value"] + "min"
+    + "<br>&emsp;等时圈内poi数目: " + positions.length + '<svg id="poisvg"><g class="xaxis"></g><g class="yaxis"></g></svg>' 
 
+    
 
-        document.getElementById("chart").innerHTML = 
-        "参数已更新<br> 当前参数：<br> &emsp;等时圈时间：" + values["time_value"] + "min"
-         + "<br>&emsp;POI: " + poidata["index"]
-        + "<br>&emsp;data: " + positions.length
-
-        /*读取数据后绘图
-        svg = d3.select("#bar-chart").select("svg")
-        svg.attr("class","firstsvg")
-
-        var w = 200;
-        var h = 100;
-        
-        var xscale = d3.scale.linear()
-        .domain(poidata["pois"])
-        .range([0,w])
-        var yscale = d3.scale.linear()
-        .domain(0,d3.max(poidata["pois"]))
-        .range(h-20,45)
-
-        var xAxis = d3.svg.axis.scale(xscale).orient("bottom")
-
-        svg.select("g.xaxis").call(xAxis)
-        .attr("transform",`translate(0, ${h-20})`)
-        .selectAll("text")
-        .attr("transform","rotate(-14)")
-
-
-
-        var bars = svg.selectAll('rect.first_bar').data(poidata["pois"]);
-        bars.exit().remove();
-
-        bars.enter().append('rect').attr('class','city-bar');
-
-        bars
-        .attr("x",function(d){return xscale(d)-15})
-        .attr("y",function(d){return yscale(d)-10})
-        .attr("width",20)
-        .attr("height",function(d){return h - yscale(d) - 10})*/
-
-
-    });
-}
+};
